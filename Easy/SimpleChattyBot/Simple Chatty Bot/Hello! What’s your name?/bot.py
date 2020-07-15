f3a8b66af5d8ba6feb52e58f2d2468a3d3338f1c
xXxAntikvarxXx <xxxantikvarxxx@gmail.com>
2020-07-15 23:10:21 +0300

    * Added new project Simple Chatty Bot

---
+++ Easy/SimpleChattyBot/Simple Chatty Bot/Hello! What’s your name?/bot.py
@@ -0,0 +1,3 @@
+# write your code here
+print("Hello! My name is Aid.")
+print("I was created in 2020.")
---
+++ Easy/SimpleChattyBot/Simple Chatty Bot/What's my name?/bot.py
@@ -0,0 +1,8 @@
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+name = input()
+
+print(f'What a great name you have, {name}!')
